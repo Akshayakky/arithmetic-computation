8d52862c92213c555be8d0b39074fe3ff021c84e
akshayakky <akshaysmpt01@gmail.com>
2020-02-13 10:29:02 +0530

    ADDED : Result Stored In Dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -x
 
-# read three numbers and assigned them to 3 vars
+#CONSTANTS
+NO_OF_COMPUTES=4
+
+#VARIABLES
+count=1
+
+#READ THREE NUMBERS AND ASSIGNED THEM TO VARIABLES
 read -p "Enter number one : " a
 read -p "Enter number two : " b
 read -p "Enter number three : " c
 
 #Computations
+echo "scale=2; $a+$b*$c" | bc
 compute1=`echo "scale=2; $a+$b*$c" | bc`
 compute2=`echo "scale=2; $a*$b+$c" | bc`
 compute3=`echo "scale=2; $c+$a/$b" | bc`
 compute4=`echo "$a%$b+$c" | bc`
+
+#Declaring Dictionary
+declare -A resultDictionary
+
+resultDictionary[compute1]=$compute1
+resultDictionary[compute2]=$compute2
+resultDictionary[compute3]=$compute3
+resultDictionary[compute4]=$compute4
+
